5b4eb71561f9083ac9d2333683867b029faa84ca
officialcjunior <realc@protonmail.com>
2020-05-07 15:06:37 +0530

    Create module4 solutions

---
+++ module4/script.js
@@ -0,0 +1,17 @@
+function (){
+  var namelist = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var name in namelist) {
+    var letter = namelist[name].charAt(0);    
+    if (letter.toLowerCase() == 'j')
+    {
+      byeSpeaker.speak(namelist[name]);
+    } 
+    else
+    { 
+      helloSpeaker.speak(namelist[name]);  
+    }
+  }
+})();
+
+
+
